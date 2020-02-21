7b9f9bf85b8a5877a1895342e2961b86d96c42e0
Conor Toomb <conor.toomb114@gmail.com>
2020-02-21 17:03:35 -0500

    improvements made

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,8 +1,16 @@
 const router = require('express').Router();
-const {notes} = require('../../data/db.json')
+const {notes} = require('../../data/db.json');
 
 router.get('/api/notes', (req, res) => {
   return res.json(notes);
+});
+
+router.post('/api/notes', (req, res) => {
+  console.log(req.body);
+
+  notes.push(req.body);
+
+  res.json(notes);
 });
 
 module.exports = router;
